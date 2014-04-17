735b24197d830c6bf1a7e73bf988746272897a7e
Branden Visser <mrvisser@gmail.com>
2014-04-17 12:52:22 -0400

    Add cluster reboot to the list of available tasks

--- fabfile/cluster/oae/__init__.py
+++ fabfile/cluster/oae/__init__.py
@@ -3,7 +3,7 @@ from fabric.tasks import execute
 from .. import hosts as cluster_hosts, util as cluster_util
 from ... import hilary, puppet, ui
 
-__all__ = ["upgrade", "upgrade_host"]
+__all__ = ["reboot", "upgrade", "upgrade_host"]
 
 
 @runs_once
