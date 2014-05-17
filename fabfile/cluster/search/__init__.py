f473a3258e4fd0e47900307d44e51b0b9ff3dce1
Branden Visser <mrvisser@gmail.com>
2014-05-17 12:55:15 -0400

    Add option to not rebuild search index right away

--- fabfile/cluster/search/__init__.py
+++ fabfile/cluster/search/__init__.py
@@ -8,7 +8,7 @@ __all__ = ["upgrade", "upgrade_host"]
 
 @runs_once
 @task
-def upgrade(refresh_data=False, uninstall=True):
+def upgrade(delete_index=False, rebuild_index=False, uninstall=True):
     """Runs through a general upgrade procedure for all known search nodes.
 
         This will:
@@ -18,13 +18,13 @@ def upgrade(refresh_data=False, uninstall=True):
             2.  Stop puppet on the search nodes
             3.  Perform a git pull on the puppet node to get the latest
                 configuration
-            4.  If `refresh_data` was `True`, delete the search index
+            4.  If `delete_index` was `True`, delete the search index
             5.  Unless `uninstall` was `False`, uninstall ElasticSearch
             6.  Do a full search cluster shut down
             7.  Run puppet on each search node
             8.  Bring the cluster back up
-            9.  If refresh_data was `True`, restart an app node to recreate the
-                search index
+            9.  If `rebuild_index` was `True`, restart an app node to recreate the
+                search index. This will not reindex all data, though
     """
     cluster_util.ensure_sudo_pass()
 
@@ -37,7 +37,7 @@ def upgrade(refresh_data=False, uninstall=True):
         execute(puppet.git_update)
 
     # If we're deleting the data, clear index
-    if refresh_data:
+    if delete_index:
         with settings(hosts=[cluster_hosts.search()[0]]):
             execute(search.delete_index, index_name=search_index_name())
 
@@ -59,7 +59,7 @@ def upgrade(refresh_data=False, uninstall=True):
 
     # If we deleted the search index, bounce an app server so it will recreate
     # the index and its mappings
-    if refresh_data:
+    if rebuild_index:
         with settings(hosts=[cluster_hosts.app()[0]]):
             execute(hilary.stop)
             execute(hilary.start)
@@ -72,7 +72,8 @@ def upgrade(refresh_data=False, uninstall=True):
 
 @runs_once
 @task
-def upgrade_host(refresh_data=False, uninstall=True, hilary_reboot_host="pp0"):
+def upgrade_host(delete_index=False, rebuild_index=False, uninstall=True,
+    hilary_reboot_host="pp0"):
     """Run through the general upgrade procedure for a search node, assuming
     puppet has already been updated.
 
@@ -80,9 +81,15 @@ def upgrade_host(refresh_data=False, uninstall=True, hilary_reboot_host="pp0"):
 
             1.  Ask for a password with which to sudo
             2.  Forcefully stop any current puppet runs
-            3.  Run puppet
-            5.  Restart elasticsearch
-            6.  Start puppet
+            3.  If `delete_index` was enabled, the search index will be deleted
+            4.  If `uninstall` was not disabled, uninstall ElasticSearch
+            5.  Ensure the search service is stopped
+            6.  Run puppet to re-install and start the search service
+            7.  Ensure the search service is started
+            8.  If `rebuild_index` was enabled, the Hilary node as set by
+                `hilary_reboot_host` will have its Hilary service restarted to
+                let it rebuild the search index
+            9.  Start the puppet service
     """
     cluster_util.ensure_sudo_pass()
 
@@ -90,7 +97,7 @@ def upgrade_host(refresh_data=False, uninstall=True, hilary_reboot_host="pp0"):
     execute(puppet.stop, force=True)
 
     # If we're deleting the data, clear the index
-    if refresh_data:
+    if delete_index:
         execute(search.delete_index, index_name=search_index_name())
 
     # Uninstall ElasticSearch if the option has not been disabled
@@ -108,7 +115,7 @@ def upgrade_host(refresh_data=False, uninstall=True, hilary_reboot_host="pp0"):
 
     # If we refreshed the data, reboot a hilary node so it can
     # create the schema again
-    if refresh_data:
+    if rebuild_index:
         with settings(hosts=[hilary_reboot_host]):
             execute(hilary.stop)
             execute(hilary.start)
